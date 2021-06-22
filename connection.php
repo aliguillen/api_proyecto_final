38d081b13bd108a4891067ce03b78d81d725d1aa
aguillen <aligguillen@gmail.com>
2021-06-22 16:17:50 -0600

    changes on sonarqube config

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "proyecto_final";
+    $con = mysqli_connect($dbhost, $dbuser, $dbpass , $db) or die($con);
+?>
